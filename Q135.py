06a74b5de82bf9dcd78317cdeca57958b2eb6ede
subhankar0002 <sahasubhankarkoronn@gmail.com>
2025-06-02 17:55:15 +0530

    Palindrome Number

---
+++ Q135.py
@@ -0,0 +1,32 @@
+'''
+Candy..............................................
+There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+You are giving candies to these children subjected to the following requirements:
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+Return the minimum number of candies you need to have to distribute the candies to the children.
+
+Example 1:
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+Example 2:
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+'''
+
+class Solution(object):
+    def candy(self, ratings):
+        
+        count=0
+        for i in range(1,len(ratings)-1):
+            if (ratings[i]>ratings[i-1] or ratings[i]>ratings[i+1]) :
+                count+=1
+        if ratings[0]>ratings[1]:
+            count+=1
+        if ratings[-1]>ratings[-2]:
+            count+=1
+        return count+len(ratings)
