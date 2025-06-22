132858321d5caac56abe232783d3479a337d7baf
subhankar0002 <sahasubhankarkoronn@gmail.com>
2025-06-22 18:28:36 +0530

    4Sum

---
+++ Q18.py
@@ -0,0 +1,52 @@
+'''
+18. 4Sum......................................
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+
+'''
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        n = len(nums)
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                k = j + 1
+                l = n - 1
+                while k < l:
+                    total = nums[i] + nums[j] + nums[k] + nums[l]
+                    if total < target:
+                        k += 1
+                    elif total > target:
+                        l -= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[k], nums[l]])
+                        while k < l and nums[k] == nums[k + 1]:
+                            k += 1
+                        while k < l and nums[l] == nums[l - 1]:
+                            l -= 1
+                        k += 1
+                        l -= 1
+        return result
